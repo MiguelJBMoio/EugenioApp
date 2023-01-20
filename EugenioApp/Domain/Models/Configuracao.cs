67cc86e63501313d3c2f34f2bc364b1750cb9c2d
MiguelMoio <migueljbmoio@hotmail.com>
2023-01-20 17:40:04 +0000

    Added

---
+++ EugenioApp/Domain/Models/Configuracao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Configuracao
+    {
+        public int PK_Configuracao { get; set; }
+        public string Titulo { get; set; }
+        public TimeOnly Tempo_Configuracao { get; set; }
+
+        public string Texto { get; set; }
+    }
+}
